b87355e155bb54a77f9df499d66850977643e8d4
Ozkan Arslan <ozkan.arslan.321@gmail.com>
2025-08-08 23:06:04 +0300

    Remove createTestUser function and update CORS configuration to allow requests from the Vercel frontend URL.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://reservation-frontend-phi.vercel.app/',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
